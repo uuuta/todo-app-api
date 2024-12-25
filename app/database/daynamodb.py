e5f624d4425bdd0d355b81e6c39ef01374390dbc
uuuta <ukattani@gmail.com>
2024-12-25 22:23:09 +0900

    refactor: モノレポ構成をやめてpoetryに移行

--- app/database/daynamodb.py
+++ app/database/daynamodb.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 import boto3
 
 
--- app/main.py
+++ app/main.py
@@ -35,4 +35,4 @@ app.add_exception_handler(HTTPException, handler=http_exception_handler)
 handler = Mangum(app)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
